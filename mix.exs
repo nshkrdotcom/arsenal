4cf0090f47a285be23104d81bfd7f710d32f09cc
nshkrdotcom <ZeroTrust@NSHkr.com>
2025-07-19 13:36:57 -1000

    ci cleanup

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Arsenal.MixProject do
     [
       app: :arsenal,
       version: @version,
-      elixir: "~> 1.14",
+      elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       description: description(),
